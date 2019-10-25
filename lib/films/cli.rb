942fb029f4f4b3c3ecd56b1fc583acf009264cc1
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-25 21:25:03 +0000

    Debugged CLI

--- lib/films/cli.rb
+++ lib/films/cli.rb
@@ -4,7 +4,6 @@ class CLI
   
   def call 
     start
-    farewell
   end
   
   def start
@@ -15,13 +14,25 @@ class CLI
       puts "Enter 'exit' to end the program."
       input = gets.strip.downcase
       @api = API.new(input)
-      @api.fetch
-      list_movies(input)
-      menu
+      if input == 'exit'
+        farewell
+      elsif input == 'all'
+        @api.fetch
+        list_movies(input)
+        menu
+      elsif input.to_i.is_a?(Integer) && input.to_i >= 1850
+        @api.fetch
+        list_movies(input)
+        menu
+      else
+        puts "**Please enter an input based on menu options**"
+        start
+      end
     end
   end
   
   def list_movies(year)
+     binding.pry
     films = Movies.all.select{|movie| movie.year == year}
       films.each.with_index do |m ,i|
       sleep(0.2)
@@ -31,13 +42,15 @@ class CLI
   
   def menu
     input = ""
-    while input != "exit"
+    sleep(1)
       puts "Enter a movie's corresponding number for more details."
       puts "Enter 'list' to see the list again."
       puts "Enter 'year' to choose a different year."
       puts "Enter 'exit' to quit the program."
       input = gets.strip.downcase
-      if input == 'list'
+      if input == 'exit'
+        farewell
+      elsif input == 'list'
         list_movies(year)
       elsif input.to_i.between?(1, 20)
         film = Movies.all[input.to_i-1]
@@ -45,8 +58,9 @@ class CLI
         display_movie(film)
       elsif input == 'year'
         start
+      else
+        puts "**Please enter an input based on menu options**"
       end
-    end
   end
   
   def display_movie(movie)
@@ -57,20 +71,26 @@ class CLI
     puts "Synopsis: #{movie.overview}"
     puts "--------------------------"
     sleep(1)
-    puts "Revenue: $#{movie.revenue}" if movie.revenue != 0
+    puts "Revenue: $#{movie.revenue}" 
     puts "--------------------------"
     sleep(1)
-    puts "Budget: $#{movie.budget}" if movie.budget != 0
+    puts "Budget: $#{movie.budget}" 
     puts "--------------------------"
     sleep(1)
     puts "Runtime: #{movie.runtime} minutes"
     puts "--------------------------"
     puts ""
-    sleep(0.5)
+    sleep(1)
   end
   
-  def unavailable #validate?
-    puts "Sorry, this movie doesn't have this information."
+  # def validate(attribute)
+  #   if attribute == 0 
+  #     unavailable
+  #   end
+  # end
+  
+  def unavailable
+    puts "No information available"
   end
   
   def farewell
